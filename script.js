5cf2cef853a4ecd65a6626a501912ecd55bd570f
Mathilda06 <udehmmesoma06@gmail.com>
2022-06-21 22:59:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var firstname="Mathilda";
+var lastname="Udeh";
+var height="5.8ft";
+var country="Nigeria"
+
+name = firstname + " " + lastname;
+
+document.write("Name:" + " " + name +"<br>")
+document.write("Height:" + " " + height +"<br>")
+document.write("Country:" + " " + country +"<br>")
